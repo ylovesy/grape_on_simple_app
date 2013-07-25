baeea8e385a45d6286cc5fd3c5a17906552a8d69
nick <nick@beansmile.com>
2013-07-25 10:17:01 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrapeOnSimpleApp::Application.config.secret_token = '8a35616b5ca6cac8ffd2bae71f72ca05d74cfc487a902727fdf2302957edba49808300518786815ff03129301a06a3e05fa5bd00d15e93e48fdcce89bcdf8bba'
